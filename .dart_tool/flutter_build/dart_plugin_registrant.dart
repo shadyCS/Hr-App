9812f23ffe00fe23b8d46b1b66bd0ea4470b3de0
ShadyCs <shady.cs@outlook.com>
2022-10-18 12:19:12 +0300

    First commit

---
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -0,0 +1,132 @@
+//
+// Generated file. Do not edit.
+// This file is generated from template in file `flutter_tools/lib/src/flutter_plugins.dart`.
+//
+
+// @dart = 2.15
+
+import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:image_picker_android/image_picker_android.dart';
+import 'package:shared_preferences_android/shared_preferences_android.dart';
+import 'package:image_picker_ios/image_picker_ios.dart';
+import 'package:shared_preferences_ios/shared_preferences_ios.dart';
+import 'package:flutter_keyboard_visibility_linux/flutter_keyboard_visibility_linux.dart';
+import 'package:shared_preferences_linux/shared_preferences_linux.dart';
+import 'package:flutter_keyboard_visibility_macos/flutter_keyboard_visibility_macos.dart';
+import 'package:shared_preferences_macos/shared_preferences_macos.dart';
+import 'package:flutter_keyboard_visibility_windows/flutter_keyboard_visibility_windows.dart';
+import 'package:shared_preferences_windows/shared_preferences_windows.dart';
+
+@pragma('vm:entry-point')
+class _PluginRegistrant {
+
+  @pragma('vm:entry-point')
+  static void register() {
+    if (Platform.isAndroid) {
+      try {
+        ImagePickerAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`image_picker_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+      try {
+        SharedPreferencesAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    } else if (Platform.isIOS) {
+      try {
+        ImagePickerIOS.registerWith();
+      } catch (err) {
+        print(
+          '`image_picker_ios` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+      try {
+        SharedPreferencesIOS.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_ios` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    } else if (Platform.isLinux) {
+      try {
+        FlutterKeyboardVisibilityPluginLinux.registerWith();
+      } catch (err) {
+        print(
+          '`flutter_keyboard_visibility_linux` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+      try {
+        SharedPreferencesLinux.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_linux` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    } else if (Platform.isMacOS) {
+      try {
+        FlutterKeyboardVisibilityPluginMacos.registerWith();
+      } catch (err) {
+        print(
+          '`flutter_keyboard_visibility_macos` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+      try {
+        SharedPreferencesMacOS.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_macos` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    } else if (Platform.isWindows) {
+      try {
+        FlutterKeyboardVisibilityPluginWindows.registerWith();
+      } catch (err) {
+        print(
+          '`flutter_keyboard_visibility_windows` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+      try {
+        SharedPreferencesWindows.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_windows` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    }
+  }
+}
--- lib/calendarscreen.dart
+++ lib/calendarscreen.dart
@@ -59,41 +59,40 @@ class _CalendarScreenState extends State<CalendarScreen> {
                   child: GestureDetector(
                     onTap: () async {
                       final month = await showMonthYearPicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(2022),
-                        lastDate: DateTime(2099),
-                        builder: (context, child) {
-                          return Theme(
-                            data: Theme.of(context).copyWith(
-                              colorScheme: ColorScheme.light(
-                                primary: primary,
-                                secondary: primary,
-                                onSecondary: Colors.white,
-                              ),
-                              textButtonTheme: TextButtonThemeData(
-                                style: TextButton.styleFrom(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2022),
+                          lastDate: DateTime(2099),
+                          builder: (context, child) {
+                            return Theme(
+                              data: Theme.of(context).copyWith(
+                                colorScheme: ColorScheme.light(
                                   primary: primary,
+                                  secondary: primary,
+                                  onSecondary: Colors.white,
+                                ),
+                                textButtonTheme: TextButtonThemeData(
+                                  style: TextButton.styleFrom(
+                                    primary: primary,
+                                  ),
+                                ),
+                                textTheme: const TextTheme(
+                                  headline4: TextStyle(
+                                    fontFamily: "NexaBold",
+                                  ),
+                                  overline: TextStyle(
+                                    fontFamily: "NexaBold",
+                                  ),
+                                  button: TextStyle(
+                                    fontFamily: "NexaBold",
+                                  ),
                                 ),
                               ),
-                              textTheme: const TextTheme(
-                                headline4: TextStyle(
-                                  fontFamily: "NexaBold",
-                                ),
-                                overline: TextStyle(
-                                  fontFamily: "NexaBold",
-                                ),
-                                button: TextStyle(
-                                  fontFamily: "NexaBold",
-                                ),
-                              ),
-                            ),
-                            child: child!,
-                          );
-                        }
-                      );
+                              child: child!,
+                            );
+                          });
 
-                      if(month != null) {
+                      if (month != null) {
                         setState(() {
                           _month = DateFormat('MMMM').format(month);
                         });
@@ -118,98 +117,111 @@ class _CalendarScreenState extends State<CalendarScreen> {
                     .doc(User.id)
                     .collection("Record")
                     .snapshots(),
-                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if(snapshot.hasData) {
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasData) {
                     final snap = snapshot.data!.docs;
                     return ListView.builder(
                       itemCount: snap.length,
                       itemBuilder: (context, index) {
-                        return DateFormat('MMMM').format(snap[index]['date'].toDate()) == _month ? Container(
-                          margin: EdgeInsets.only(top: index > 0 ? 12 : 0, left: 6, right: 6),
-                          height: 150,
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black26,
-                                blurRadius: 10,
-                                offset: Offset(2, 2),
-                              ),
-                            ],
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                child: Container(
-                                  margin: const EdgeInsets.only(),
-                                  decoration: BoxDecoration(
-                                    color: primary,
-                                    borderRadius: const BorderRadius.all(Radius.circular(20)),
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      DateFormat('EE\ndd').format(snap[index]['date'].toDate()),
-                                      style: TextStyle(
-                                        fontFamily: "NexaBold",
-                                        fontSize: screenWidth / 18,
-                                        color: Colors.white,
-                                      ),
+                        return DateFormat('MMMM')
+                                    .format(snap[index]['date'].toDate()) ==
+                                _month
+                            ? Container(
+                                margin: EdgeInsets.only(
+                                    top: index > 0 ? 12 : 0, left: 6, right: 6),
+                                height: 150,
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black26,
+                                      blurRadius: 10,
+                                      offset: Offset(2, 2),
                                     ),
-                                  ),
+                                  ],
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20)),
                                 ),
-                              ),
-                              Expanded(
-                                child: Column(
+                                child: Row(
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
-                                    Text(
-                                      "Check In",
-                                      style: TextStyle(
-                                        fontFamily: "NexaRegular",
-                                        fontSize: screenWidth / 20,
-                                        color: Colors.black54,
+                                    Expanded(
+                                      child: Container(
+                                        margin: const EdgeInsets.only(),
+                                        decoration: BoxDecoration(
+                                          color: primary,
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(20)),
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                            DateFormat('EE\ndd').format(
+                                                snap[index]['date'].toDate()),
+                                            style: TextStyle(
+                                              fontFamily: "NexaBold",
+                                              fontSize: screenWidth / 18,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
                                       ),
                                     ),
-                                    Text(
-                                      snap[index]['checkIn'],
-                                      style: TextStyle(
-                                        fontFamily: "NexaBold",
-                                        fontSize: screenWidth / 18,
+                                    Expanded(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            "Check In",
+                                            style: TextStyle(
+                                              fontFamily: "NexaRegular",
+                                              fontSize: screenWidth / 20,
+                                              color: Colors.black54,
+                                            ),
+                                          ),
+                                          Text(
+                                            snap[index]['checkIn'],
+                                            style: TextStyle(
+                                              fontFamily: "NexaBold",
+                                              fontSize: screenWidth / 18,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            "Check Out",
+                                            style: TextStyle(
+                                              fontFamily: "NexaRegular",
+                                              fontSize: screenWidth / 20,
+                                              color: Colors.black54,
+                                            ),
+                                          ),
+                                          Text(
+                                            snap[index]['checkOut'],
+                                            style: TextStyle(
+                                              fontFamily: "NexaBold",
+                                              fontSize: screenWidth / 18,
+                                            ),
+                                          ),
+                                        ],
                                       ),
                                     ),
                                   ],
                                 ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      "Check Out",
-                                      style: TextStyle(
-                                        fontFamily: "NexaRegular",
-                                        fontSize: screenWidth / 20,
-                                        color: Colors.black54,
-                                      ),
-                                    ),
-                                    Text(
-                                      snap[index]['checkOut'],
-                                      style: TextStyle(
-                                        fontFamily: "NexaBold",
-                                        fontSize: screenWidth / 18,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ) : const SizedBox();
+                              )
+                            : const SizedBox();
                       },
                     );
                   } else {
